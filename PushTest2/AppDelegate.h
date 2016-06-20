3091281cd464b6971899ca59230525b2c6b7a5f9
F839-002 M <mf839-002@70-12-243-251.pools.spcsdns.net>
2016-06-20 17:00:58 +0900

    Initial Commit

---
+++ PushTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushTest2
+//
+//  Created by MF839-002 on 2016. 6. 20..
+//  Copyright © 2016년 MF839-002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushTest2
+//
+//  Created by MF839-002 on 2016. 6. 20..
+//  Copyright © 2016년 MF839-002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
